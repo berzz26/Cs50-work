88766af34ec3020cc1c6a441e46112b68ffa522d
Aum Tamboli <aumtamboli2233@gmail.com>
2024-04-16 22:29:05 +0530

    Create pattern2.c
    the output would be -
       #  #
      ##  ##
     ###  ###
    ####  ####

---
+++ pattern2.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int main(void)
+{
+    int rows = 4;
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < rows - i - 1; j++)
+        {
+            printf(" ");
+        }
+        for (int k = 0; k <= i; k++)
+        {
+            printf("#");
+        }
+
+        for (int l = 0; l < 2; l++)
+        {
+            printf(" ");
+        }
+
+        for (int m = 0; m <= i; m++)
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
+}
